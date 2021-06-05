7f803a3a61e2af0562b46510b0f8863c1e646d20
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-06-05 11:11:06 +0300

    @Autowired added in LanguagesController

--- hrms/src/main/java/com/javareact/hrms/api/controllers/LanguagesController.java
+++ hrms/src/main/java/com/javareact/hrms/api/controllers/LanguagesController.java
@@ -2,6 +2,7 @@ package com.javareact.hrms.api.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ public class LanguagesController {
 
 	private LanguageForCvService laguageForCvService;
 
+	@Autowired
 	public LanguagesController(LanguageForCvService laguageForCvService) {
 		super();
 		this.laguageForCvService = laguageForCvService;
